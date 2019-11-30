4495b2da2e522bfccd706d15f0578df721d377a4
Peter Elliott <pelliott@official-sounding.com>
2019-11-30 12:26:15 -0600

    initial implementation - translate happy-path WS events

---
+++ crc-jamtimer/CRGMessageProcessor.cs
@@ -0,0 +1,144 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Websocket.Client;
+
+namespace crc_jamtimer
+{
+    public class CRGMessageProcessor
+    {
+        private string RegistrationStrings { get => @$"{{
+                    ""action"": ""Register"",
+                    ""paths"": [
+                        ""{IN_JAM}"",
+                        ""{LINEUP_DIRECTION}"",
+                        ""{LINEUP_UP_TIME}"",
+                        ""{LINEUP_DOWN_TIME}"",
+                        ""{TIMEOUT_RUNNING}""
+                ]}}"; 
+        }
+
+        public string PingString { get => "{ \"action\": \"Ping\" }"; }
+
+        private bool initialized = false;
+        private bool inJam = false;
+        private bool inTimeout = false;
+        private bool lineupInverted = true;
+
+        public async Task Initialize(WebsocketClient client)
+        {
+            initialized = false;
+            await client.Send(RegistrationStrings);
+        }
+
+        public void Process(ResponseMessage msg)
+        {
+            var obj = JObject.Parse(msg.Text);
+            if (obj.ContainsKey("state"))
+            {
+                var state = obj["state"].ToObject<Dictionary<string, object>>();
+
+                if (state.ContainsKey(TIMEOUT_RUNNING))
+                {
+                    bool timeoutStatus = state[TIMEOUT_RUNNING] as bool? ?? false;
+                    if (inTimeout != timeoutStatus)
+                    {
+                        inTimeout = timeoutStatus;
+                        if (!inTimeout)
+                        {
+                            TriggerEvent(InGameEvent.EndOfTimeout);
+                        }
+                        else if (!inJam)
+                        {
+                            TriggerEvent(InGameEvent.TimeoutDuringLineup);
+                        }
+
+                    }
+                }
+
+                if (state.ContainsKey(IN_JAM))
+                {
+                    bool jamStatus = state[IN_JAM] as bool? ?? false;
+
+                    if (jamStatus != inJam)
+                    {
+                        inJam = jamStatus;
+                        if (jamStatus)
+                        {
+                            TriggerEvent(InGameEvent.JamStarted);
+                        }
+                        else
+                        {
+                            TriggerEvent(InGameEvent.JamEnded);
+                        }
+                    }
+                }
+
+                if (state.ContainsKey(LINEUP_DIRECTION))
+                {
+                    lineupInverted = state[LINEUP_DIRECTION] as bool? ?? false;
+                }
+
+                var lineupTime = lineupInverted ?  LINEUP_DOWN_TIME : LINEUP_UP_TIME;
+                if (state.ContainsKey(lineupTime))
+                {
+                    var time = state[lineupTime] as long? ?? 0;
+                    if (time == 25000)
+                    {
+                        TriggerEvent(InGameEvent.FiveSeconds);
+                    }
+                }
+
+                if (!initialized)
+                {
+                    initialized = true;
+                    Console.WriteLine("Initialized State");
+                }
+            }
+        }
+
+        private void TriggerEvent(InGameEvent ev) {
+            if(initialized)
+            {
+                switch(ev)
+                {
+                    case InGameEvent.FiveSeconds:
+                        Console.WriteLine("FIVE SECONDS!");
+                        break;
+                    case InGameEvent.JamStarted:
+                        Console.WriteLine("Start Jam");
+                        break;
+                    case InGameEvent.JamEnded:
+                        Console.WriteLine("End Jam");
+                        break;
+                    case InGameEvent.TimeoutDuringLineup:
+                        Console.WriteLine("Timeout");
+                        break;
+                    case InGameEvent.EndOfTimeout:
+                        Console.WriteLine("End of Timeout");
+                        break;
+                }
+            }
+        }
+
+        private static string IN_JAM = "ScoreBoard.InJam";
+        private static string LINEUP_DIRECTION = "ScoreBoard.Clock(Lineup).Direction";
+        private static string LINEUP_UP_TIME = "ScoreBoard.Clock(Lineup).Time";
+        private static string LINEUP_DOWN_TIME = "ScoreBoard.Clock(Lineup).InvertedTime";
+        private static string TIMEOUT_RUNNING = "ScoreBoard.Clock(Timeout).Running";
+    }
+
+
+    public enum InGameEvent
+    {
+        FiveSeconds,
+        JamStarted,
+        JamEnded,
+        TimeoutDuringJam,
+        TimeoutDuringLineup,
+        EndOfTimeout
+    }
+}
---
+++ crc-jamtimer/Program.cs
@@ -0,0 +1,81 @@
+﻿using Fclp;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Websocket.Client;
+
+namespace crc_jamtimer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var p = BuildArgParser();
+
+            var result = p.Parse(args);
+
+            if (!result.HasErrors)
+            {
+                var argObj = p.Object;
+
+                using var exitEvent = new ManualResetEvent(false);
+                var processor = new CRGMessageProcessor();
+
+                
+
+                using var client = new WebsocketClient(argObj.WS)
+                {
+                    ReconnectTimeoutMs = (int)TimeSpan.FromSeconds(30).TotalMilliseconds
+                };
+
+
+
+                client.ReconnectionHappened.Subscribe(async type => {
+                    Console.WriteLine($"Reconnection happened, type: {type}");
+                    await processor.Initialize(client);
+                });
+
+                client.MessageReceived.Subscribe(processor.Process);
+
+                await client.Start();
+
+                Console.WriteLine($"Connected to {argObj.WS}");
+
+                await processor.Initialize(client);
+                InitTimer((state) => client.Send(processor.PingString));
+
+                exitEvent.WaitOne();
+            }
+        }
+
+        private static Timer InitTimer(TimerCallback cb)
+        {
+            return new Timer(cb, null, 0, 10000);
+        }
+
+        private static FluentCommandLineParser<ApplicationArguments> BuildArgParser()
+        {
+            var p = new FluentCommandLineParser<ApplicationArguments>();
+
+            p.Setup(arg => arg.Hostname)
+                .As('h', "hostname")
+                .WithDescription("Hostname the CRG Server is running on")
+                .SetDefault("localhost");
+
+            p.Setup(arg => arg.Port)
+                .As('p', "port")
+                .WithDescription("Port the CRG Server is running on")
+                .SetDefault(8000);
+
+            return p;
+        }
+    }
+
+    public class ApplicationArguments
+    {
+        public string Hostname { get; set; }
+        public int Port { get; set; }
+
+        public Uri WS => new Uri($"ws://{Hostname}:{Port}/WS");
+    }
+}
